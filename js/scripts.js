fb5a73e265691983f77bb8410052381e1cc8efed
Alma <a.q.frankenstein@gmail.com>
2020-08-18 14:30:26 -0700

    add palindrome form and function
    Co-authored-by: Janet Karpenske <janetkarpenske@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,6 @@
 //   return userNumber * factorial(userNumber - 1);
 // }
 
-
 function factorial(userNumber) {
   if (userNumber === 0) {
     return 1;
@@ -12,13 +11,26 @@ function factorial(userNumber) {
   if (userNumber < 0) {
     return;
   }
-
   let total = 1;
   for (let num = userNumber; num > 0; num -= 1) {
     console.log(num);
     total *= num;
   }
   return total;
+}
+
+function isPalindrome(phrase){
+  let output;
+  let reversedPhrase = phrase.split("").reverse().join('');
+  console.log(reversedPhrase);
+  if (reversedPhrase === phrase) {
+    output = "true";
+    return output;
+  }
+  else {
+    output = "false";
+    return output;
+  }
 }
 
 $(document).ready(function() {
@@ -31,7 +43,17 @@ $(document).ready(function() {
 
     $("#returnNumber").text(returnNumber);
     $("#returnNumber").show();
-
   })
+
+  $("#palinTester").submit(function(event) {
+    event.preventDefault();
+
+    const phrase = $("#inputpal").val();
+
+    let palindrome = isPalindrome(phrase);
+
+    $("#palindromeAnswer").text(palindrome);
+    $("#palindromeAnswer").show();
+})
 })
 
